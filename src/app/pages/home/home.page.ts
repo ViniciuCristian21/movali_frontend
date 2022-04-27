982e81a4cccd9f82dd4a05a75389c293eaa96dde
Viniciu Cristian <git.viniciu@gmail.com>
2022-04-27 14:15:43 -0300

    update to requests get and post

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
+import { RequestsService } from './../../services/requests.service';
 import { Component, OnInit } from '@angular/core';
+import { api } from 'src/app/services/api';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  allMovies: any[] = [];
   constructor() { }
 
   ngOnInit() {
+    this.getAllMovies()
   }
 
+  getAllMovies() {
+    api.get('/movies').then( res => {
+      res.data.forEach(element => {
+        this.allMovies.push({
+          classification: element.classification,
+          description: element.description,
+          id: element.id,
+          launched_in: element.launched_in,
+          title: element.title,
+        })
+      });
+    })
+    .catch( err => console.log(err.message))
+    .finally(()=> console.log(this.allMovies))
+  }
 }
--- src/app/pages/movies-form/movies-form.page.ts
+++ src/app/pages/movies-form/movies-form.page.ts
@@ -1,4 +1,6 @@
+import { api } from './../../services/api';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movies-form',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-form.page.scss'],
 })
 export class MoviesFormPage implements OnInit {
-
-  constructor() { }
+  note: number = 0;
+  name: string = '';
+  email: string = '';
+  id: string = '';
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.id = this.activatedRoute.snapshot.params['id'];
+  }
+
+  onSubmit() {
+    console.log({
+      note: Number(this.note),
+      name: this.name,
+      email: this.email,
+      movieId: this.id
+    })
+
+    api.post("/new-evaluation", {
+      note: Number(this.note),
+      name: this.name,
+      email: this.email,
+      id_movie: this.id
+    })
+    .then( res => console.log(res.data))
+    .catch(err => console.log(err.message))
+    .finally( ()=> this.router.navigate(['/home/']))
   }
 
 }
